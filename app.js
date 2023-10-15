8ff8876d0d900558bf39ac7a9c6f86c271f43763
Nguyen Huy <huy246302@gmail.com>
2023-10-15 23:30:32 +0700

    client commit

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'I am a message from Server!'});
-})
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(4000, () => console.log('App listening on port 4000'));
